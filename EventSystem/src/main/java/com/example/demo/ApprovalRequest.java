1e8e273db05b9bfc23def8a43a9319d32b8f7aa3
Dpapadopoulou <116271267+Dpapadopoulou@users.noreply.github.com>
2025-01-11 14:32:59 +0200

    new
    new

--- EventSystem/src/main/java/com/example/demo/ApprovalRequest.java
+++ EventSystem/src/main/java/com/example/demo/ApprovalRequest.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 public class ApprovalRequest {
-	//private String type; 
+	private String type; 
 	private Event theEvent;
 	private Organizer SubmittedBy; //AFM of the organizer that made the request
 	private LocalDate createdAt; //date that the request was created
@@ -14,14 +14,28 @@ public class ApprovalRequest {
 	private String comments;
 	
 	//request is created on the event constractor   
-	public ApprovalRequest(Event theEvent, Organizer submittedBy, LocalDate createdAt, String status) {
-		//this.type = type;
+	public ApprovalRequest(Event theEvent, Organizer submittedBy, LocalDate createdAt, String status, String type) {
+		this.type = type;
 		this.theEvent = theEvent;
 		this.SubmittedBy = submittedBy;
 		this.createdAt = createdAt;
 		this.status = status;		
 	}
 	
+	
+	
+	public String getType() {
+		return type;
+	}
+
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+
 	public Event getTheEvent() {
 		return theEvent;
 	}
@@ -70,5 +84,16 @@ public class ApprovalRequest {
 		this.setClosedAt(LocalDate.now());
 		this.setComments(comment);
 	}
+
+
+
+	@Override
+	public String toString() {
+		return "ApprovalRequest [type=" + type + ", theEvent=" + theEvent + ", SubmittedBy=" + SubmittedBy
+				+ ", createdAt=" + createdAt + ", status=" + status + ", handledBy=" + handledBy + ", closedAt="
+				+ closedAt + ", comments=" + comments + "]";
+	}
+	
+	
 	
 }
